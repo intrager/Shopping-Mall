bdabcbe7d36920b4341a63b35cc73dc4de9cc0c0
Bruce Han <intrager@naver.com>
2022-08-25 23:46:51 +0900

    Layout configuration with Thymeleaf

---
+++ ecommerce/src/main/java/com/personal/domain/ItemDto.java
@@ -0,0 +1,18 @@
+package com.personal.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private String itemDetail;
+    private String sellStat;
+    private LocalDateTime regDate;
+    private LocalDateTime upDate;
+}
